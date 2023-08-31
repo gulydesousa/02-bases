4a8f38005c4fd0b4aaad3f68a226cfb9299ca198
Guly de Sousa <gdesousa@gmail.com>
2023-08-31 20:39:54 +0200

    Angular de cero a experto - (Angular 8+)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = '02-bases';
+
+export class AppComponent implements OnInit {
+  public title:string = 'Mi primera app en a Angular';
+
+  constructor() {  }
+
+  ngOnInit(): void {  }
+
 }
